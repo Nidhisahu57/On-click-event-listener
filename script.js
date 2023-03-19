71447f1716b7f557a046917b38a8e441962144f5
Nidhisahu57 <110117913+Nidhisahu57@users.noreply.github.com>
2023-03-19 16:05:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("btn");
+
+function magic(){
+    document.querySelector("h1").innerHTML = "I am happy to learn with 'PW Skills'.";
+    document.querySelector("h1").style.color = "#0E5E6F";
+    
+};
+
+btn.addEventListener("click", magic);
